c1929a7020b67942cf055f1d851f02fd9ef8ead0
Philipp Brumm <philipp.brumm@gmail.com>
2020-03-29 19:33:24 +0200

    introduce useMountedCallback from react-query to work work around accidentally updating an unmounted component

--- src/hooks.js
+++ src/hooks.js
@@ -1,18 +1,39 @@
 import React from 'react'
 import { queryCache, stableStringify } from 'react-query'
 
+export const useMountedCallback = callback => {
+  const mounted = React.useRef(false)
+
+  React.useLayoutEffect(() => {
+    mounted.current = true
+    return () => (mounted.current = false)
+  }, [])
+
+  return React.useCallback(
+    (...args) => (mounted.current ? callback(...args) : void 0),
+    [callback]
+  )
+}
+
 export const useQueryState = queryKey => {
   const [state, setState] = React.useState({})
   const queryHash = stableStringify(queryKey)
 
-  React.useEffect(() => queryCache.subscribe(() => setState({})), [])
+  useMountedCallback(() => queryCache.subscribe(() => setState({})), [])
 
-  return React.useMemo(
-    () => {
-      const queryKey = JSON.parse(queryHash)
-      const query = queryCache.getQuery(queryKey)
-      return (query && query.state) || {}
-    },
-    [state, queryHash]
-  )
+  return React.useMemo(() => {
+    const queryKey = JSON.parse(queryHash)
+    const query = queryCache.getQuery(queryKey)
+    // `state` is only here so eslint shuts up
+    return (query && state && query.state) || {}
+  }, [state, queryHash])
+}
+
+export const useIdleCallback = (userFn, deps = []) => {
+  const userCallback = useMountedCallback(userFn, deps)
+
+  React.useEffect(() => {
+    const handle = window.requestIdleCallback(userCallback)
+    return () => window.cancelIdleCallback(handle)
+  }, [userCallback])
 }
