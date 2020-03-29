7bea93518cc51c9a40e719a976c69aefb36e8252
Philipp Brumm <philipp.brumm@gmail.com>
2020-03-29 16:19:42 +0200

    prefer insertAdjacentElement over insertBefore

--- src/PrependPortal.js
+++ src/PrependPortal.js
@@ -2,28 +2,23 @@ import React from 'react'
 import { createPortal } from 'react-dom'
 
 const PrependPortal = ({ targetSelector, children }) => {
-  const root = React.useRef(
-    document.querySelector(targetSelector).closest('.Box.mb-3.Box--condensed')
-  )
   const container = React.useRef(document.createElement('div'))
 
   React.useEffect(
     () => {
       const containerElement = container.current
-      const rootElement = root.current
 
       try {
-        rootElement.insertBefore(
-          containerElement,
-          document.querySelector(targetSelector)
-        )
+        document
+          .querySelector(targetSelector)
+          .insertAdjacentElement('beforebegin', containerElement)
       } catch (error) {
         console.log(error)
         debugger
       }
 
       return () => {
-        rootElement.removeChild(containerElement)
+        containerElement.remove()
       }
     },
     [targetSelector]
