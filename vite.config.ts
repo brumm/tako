e831c6552c7a76f43f4ec9559863ec7e1bafd64a
Philipp Brumm <philipp.brumm@gmail.com>
2025-12-05 21:41:55 +0100

    fix popup.html not working in dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import { crx } from '@crxjs/vite-plugin'
 import manifest from './src/manifest.json'
 
 export default defineConfig({
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+  },
   plugins: [
     react(),
     crx({ manifest }),
